585f37b18e875faa1b8fb3603550303bdcd5d081
music-brain88 <music.brain88@gmail.com>
2020-07-26 20:51:46 +0900

    Add validation

--- src/second.rs
+++ src/second.rs
@@ -8,13 +8,16 @@ fn do_sort<T: Ord>(x: &mut [T], up: bool){
         sub_sort(x, up);
     }
 }
-
-pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder){
-    match *order {
-        SortOrder::Ascending => do_sort(x,true),
-        SortOrder::Descending => do_sort(x, false),
-    };
-
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+    if x.len().is_power_of_two() {
+        match *order {
+            SortOrder::Ascending => do_sort(x,true),
+            SortOrder::Descending => do_sort(x, false),
+        };
+        Ok(())
+    } else {
+        Err(format!("The length of x is not power of two. (x.len: {})", x.len()))
+    }
 }
 
 fn sub_sort<T: Ord>(x: &mut [T], up: bool){
@@ -34,6 +37,7 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool){
         }
     }
 }
+
 #[cfg(test)]
 mod tests {
     use super::sort;
@@ -42,29 +46,35 @@ mod tests {
     #[test]
     fn sort_u32_ascending() {
         let mut x: Vec<u32> =  vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x[..], &Ascending);
+        assert_eq!(sort(&mut x[..], &Ascending), Ok(()));
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
     #[test]
     fn sort_u32_decending() {
         let mut x: Vec<u32> =  vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x[..], &Descending);
+        assert_eq!(sort(&mut x[..], &Descending), Ok(()));
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
 
     #[test]
     fn sort_str_asending() {
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, &Ascending);
+        assert_eq!(sort(&mut x[..], &Ascending), Ok(()));
         assert_eq!(x, vec!["GC", "Rust", "and", "fast", "is", "memory-efficient", "no", "with"]);
     }
 
     #[test]
     fn sort_str_desending() {
         let mut x = vec!["Rust", "is", "fast", "and", "memory-efficient", "with", "no", "GC"];
-        sort(&mut x, &Descending);
+        assert_eq!(sort(&mut x[..], &Descending), Ok(()));
         assert_eq!(x, vec!["with", "no", "memory-efficient", "is", "fast", "and", "Rust", "GC"]);
+    }
+
+    #[test]
+    fn sort_to_fail() {
+        let mut x = vec![10, 30, 11];
+        assert!(sort(&mut x, &Ascending).is_err());
     }
 
 }
